4251e916d7d718df2cd04e8ac9006246bb0f67dc
Ash <valleyknight@protonmail.com>
2020-07-06 06:30:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+## gzdoom-discordrpc
+
+![license](https://img.shields.io/badge/license-public%20domain-green)
+
+A discordrpc client for GZDoom written in Rust.
+
+I'm making this because:
+
+1. I could not find a pre-existing DiscordRPC client for it
+2. This is something I would actively use, which would make developing it that much more fun
+3. I can actually visualize how I would go about it, unlike most other projects where I feel blind as a bat
+
+## How it works
+
+1. Program reads first argument
+2. Program connects to Discord via RPC
+3. It then looks for a doom process based on supplied argument, and parses it's window title ("level - game/mod")
+4. It is separated into an &str vector
+5. The icon will be the logo for the game/mod, the status will be the level, and the hover text will be the game/mod
+6. Program loops every 15 secs or so, looping every second would be overkill
+
+## Running
+
+1. Ensure that `wmctrl` is installed, it is needed for getting a list of windows and searching for DOOM
+2. `cargo run -- doom` (vanilla DOOM, should work for all versions) or `cargo run -- pb` (Project Brutality)
+
+## Screenshots
+
+DOOM:
+
+![DOOM](images/doom.png?raw=true "DOOM")
+
+DOOM II:
+
+![DOOM II](images/doom_ii.png?raw=true "DOOM II")
+
+Project Brutality:
+
+![Project Brutality](images/pb.png?raw=true "Project Brutality")
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use discord_rpc_client::Client;
+use std::{env, thread, time};
+use wmctrl;
+
+fn main() {
+    // Input game (doom or pb)
+    let game_input = env::args().nth(1).expect("Requires at least one argument");
+
+    // Create the client
+    let mut drpc = Client::new(729549945408979065);
+
+    // Start up the client connection, so that we can actually send and receive stuff
+    drpc.start();
+    loop {
+        // Get list of windows
+        let windows = wmctrl::get_windows();
+        // if game_input = doom; then
+        //     search for DOOM process
+        // else if game_input = pb; then
+        //     search for Project Brutality process
+        // else
+        //     search for gzdoom process
+        // fi
+        let game_process = if game_input == "doom" {
+            wmctrl::utils::find_window_by_title(&windows, "DOOM").unwrap()
+        } else if game_input == "pb" {
+            wmctrl::utils::find_window_by_title(&windows, "Project Brutality").unwrap()
+        } else {
+            wmctrl::utils::find_window_by_title(&windows, "gzdoom").unwrap()
+        };
+        // Obtain window title
+        let window_title = game_process.title();
+        // Create vector out of title, using " - " as the delimiter
+        let game_vec: Vec<&str> = window_title.split(" - ").collect();
+        let level = game_vec[0];
+        let game = game_vec[1];
+        let status_game = ["Game: ".to_string(), game.to_string()].concat();
+        let status_level = ["Level: ".to_string(), level.to_string()].concat();
+        // Print the status to stdout
+        println!("{}\n{}\n--------------------", status_game, status_level);
+
+        // Get the icon
+        let icon = match game {
+            "DOOM Registered" => "doom",
+            "DOOM 2: Hell on Earth" => "doom_ii",
+            "Project Brutality" => "pb_icon",
+            "Project Brutality 3.0" => "pb_icon",
+            _ => "icon",
+        };
+
+        // Set the activity
+        if let Err(why) = drpc.set_activity(|a| {
+            a.details(status_game)
+                .state(status_level)
+                .assets(|ass| ass.large_image(icon).large_text(game))
+        }) {
+            println!("Failed to set presence: {}", why);
+        }
+
+        // Loop every 15 seconds
+        thread::sleep(time::Duration::from_secs(15));
+        println!("program has looped");
+    }
+}

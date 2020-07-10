54abeaa4b68e5897e4c47a76a8d6be5cb68d5067
Ash <valleyknight@protonmail.com>
2020-07-10 10:50:56 -0400

    make some stuff better

--- src/main.rs
+++ src/main.rs
@@ -7,59 +7,63 @@ fn info(connection: &Connection, game_input: String) -> (String, String, String)
     let windows: Vec<String> = connection.window_titles().unwrap();
     // Yes. I know. This code is bad. I don't care. It took so many hours of agony to get it to work.
     // Wait wait wait! I'm sorry, I didn't mean it! Don't go... ;-;
-    if game_input == "doom" {
-        let window = windows
-            .iter()
-            .find(|title| {
-                title
-                    .to_string()
-                    .contains("- DOOM".to_string().as_str())
-            })
-            .unwrap();
-        // "level - game" => ["level", "game"]
-        let game_vec: Vec<&str> = window.split(" - ").collect();
-        let level = game_vec[0];
-        let game = game_vec[1];
-        let status_game = ["Game: ".to_string(), game.to_string()].concat();
-        let status_level = ["Level: ".to_string(), level.to_string()].concat();
-        // Print the status to stdout
-        println!("{}\n{}\n--------------------", status_game, status_level);
-        // Return the variables
-        (game.to_string(), status_game, status_level)
-    } else if game_input == "pb" {
-        let window = windows
-            .iter()
-            .find(|title| {
-                title
-                    .to_string()
-                    .contains("- Project Brutality".to_string().as_str())
-            })
-            .unwrap();
-        let game_vec: Vec<&str> = window.split(" - ").collect();
-        let level = game_vec[0];
-        let game = game_vec[1];
-        let status_game = ["Game: ".to_string(), game.to_string()].concat();
-        let status_level = ["Level: ".to_string(), level.to_string()].concat();
-        println!("{}\n{}\n--------------------", status_game, status_level);
-        (game.to_string(), status_game, status_level)
-    } else {
-        // Generically search for gzdoom if wrong arg is supplied
-        let window = windows
-            .iter()
-            .find(|title| {
-                title
-                    .to_string()
-                    .contains("gzdoom".to_string().as_str())
-            })
-            .unwrap();
-        let game_vec: Vec<&str> = window.split(" - ").collect();
-        let level = game_vec[0];
-        let game = game_vec[1];
-        let status_game = ["Game: ".to_string(), game.to_string()].concat();
-        let status_level = ["Level: ".to_string(), level.to_string()].concat();
-        // Print the status to stdout
-        println!("{}\n{}\n--------------------", status_game, status_level);
-        (game.to_string(), status_game, status_level)
+    match game_input.as_str() {
+        "doom" => {
+            let window = windows
+                .iter()
+                .find(|title| {
+                    title
+                        .to_string()
+                        .contains("- DOOM".to_string().as_str())
+                })
+                .unwrap();
+            // "level - game" => ["level", "game"]
+            let game_vec: Vec<&str> = window.split(" - ").collect();
+            let level = game_vec[0];
+            let game = game_vec[1];
+            let status_game = ["Game: ".to_string(), game.to_string()].concat();
+            let status_level = ["Level: ".to_string(), level.to_string()].concat();
+            // Print the status to stdout
+            println!("{}\n{}\n--------------------", status_game, status_level);
+            // Return the variables
+            (game.to_string(), status_game, status_level)
+        }
+        "pb" => {
+            let window = windows
+                .iter()
+                .find(|title| {
+                    title
+                        .to_string()
+                        .contains("- Project Brutality".to_string().as_str())
+                })
+                .unwrap();
+            let game_vec: Vec<&str> = window.split(" - ").collect();
+            let level = game_vec[0];
+            let game = game_vec[1];
+            let status_game = ["Game: ".to_string(), game.to_string()].concat();
+            let status_level = ["Level: ".to_string(), level.to_string()].concat();
+            println!("{}\n{}\n--------------------", status_game, status_level);
+            (game.to_string(), status_game, status_level)
+        }
+        _ => {
+            // Generically search for gzdoom if wrong arg is supplied
+            let window = windows
+                .iter()
+                .find(|title| {
+                    title
+                        .to_string()
+                        .contains("gzdoom".to_string().as_str())
+                })
+                .unwrap();
+            let game_vec: Vec<&str> = window.split(" - ").collect();
+            let level = game_vec[0];
+            let game = game_vec[1];
+            let status_game = ["Game: ".to_string(), game.to_string()].concat();
+            let status_level = ["Level: ".to_string(), level.to_string()].concat();
+            // Print the status to stdout
+            println!("{}\n{}\n--------------------", status_game, status_level);
+            (game.to_string(), status_game, status_level)
+        }
     }
 }
 
@@ -77,10 +81,9 @@ fn main() {
     let connection = Connection::new().unwrap();
     loop {
         let (game, status_game, status_level) = info(&connection, game_input.clone());
-        let game_match = game.as_str();
 
         // Get the icon
-        let icon = match game_match {
+        let icon = match game.as_str() {
             "DOOM Registered" => "doom",
             "DOOM 2: Hell on Earth" => "doom_ii",
             "The Ultimate DOOM" => "ultimate_doom",
